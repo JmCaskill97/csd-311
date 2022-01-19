1986375de295bc5c97083be1ec642aa0633b5070
JmCaskill2077 <95253188+JmCaskill2077@users.noreply.github.com>
2022-01-18 20:34:11 -0500

    mongodb_test

--- module_5/mongodb_test.py
+++ module_5/mongodb_test.py
@@ -1,24 +1,12 @@
-""" 
-    Title: mongodb_test.py
-    Author: Professor Krasso
-    Date: 10 July 2020
-    Description: Test program for connecting to a 
-                 MongoDB Atlas cluster
-"""
 
-""" import statements """
 from pymongo import MongoClient
 
-# MongoDB connection string 
 url = "mongodb+srv://admin:admin@cluster0.rsnru.mongodb.net/pytech?retryWrites=true&w=majority"
 
-# connect to the MongoDB cluster 
 client = MongoClient(url)
 
-# connect pytech database
 db = client.pytech
 
-# show the connected collections 
 print("\n -- Pytech COllection List --")
 print(db.list_collection_names())
 
